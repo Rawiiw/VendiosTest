c39013f38a6cd97a40fd602a28265501fe6dd8b1
Alexandra <79237095+Rawiiw@users.noreply.github.com>
2024-03-02 16:58:08 +0700

    Add files via upload

---
+++ VendiosRuntime/TimerBackgroundTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml;
+
+namespace VendiosRuntime
+{
+    //интерфейс для фоновй задачи
+    public sealed class TimerBackgroundTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral; //объект отложенной задачи
+        private int _seconds = 60; 
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            //гетаем объект отложенной задачи для разрешения выполнения фоновой задачи
+            _deferral = taskInstance.GetDeferral();
+
+            //инициализируем таймер, который срабатывает каждую секунду.
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+
+            //подписываемся на событие таймера.
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            _seconds--;
+
+            if (_seconds <= 0)
+            {
+                _deferral.Complete();
+            }
+        }
+    }
+}
