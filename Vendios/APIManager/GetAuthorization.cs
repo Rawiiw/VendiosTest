ab45ecc37b9dbe8c2c398de39a28817762b071a9
Alexandra <79237095+Rawiiw@users.noreply.github.com>
2024-03-02 16:57:37 +0700

    Add files via upload

---
+++ Vendios/APIManager/GetAuthorization.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendios.APIManager
+{
+    internal class GetAuthorization
+    {
+        private const string AuthorizationUrl = "https://ferma-test.ofd.ru/api/Authorization/CreateAuthToken";
+
+        //выполнение авторизации асинхронно
+        public async Task<string> PerformAuthorizationAsync(string login, string password)
+        {
+            //создание нового экземпляра для выполнения запросов
+            var client = new HttpClient();
+
+            //создание содержимого запроса
+            var content = new StringContent($"{{ \"Login\": \"{login}\", \"Password\": \"{password}\" }}", Encoding.UTF8, "application/json");
+
+            try
+            {
+                //отправка пост-запроса на сервер авторизации
+                var response = await client.PostAsync(AuthorizationUrl, content);
+
+                //убеждаемся что ответ успешный (статус код 200)
+                response.EnsureSuccessStatusCode();
+
+                //чтение ответа от сервера
+                string result = await response.Content.ReadAsStringAsync();
+
+                //вывод результата в консоль
+                Console.WriteLine($"Результат: {result}");
+
+                //возвращаем результат авторизации
+                return result;
+            }
+            catch (Exception ex)
+            {
+                //выводим сообщение об ошибке в консоль
+                Console.WriteLine($"Ошибка: {ex.Message}");
+
+                //возбуждение исключения
+                throw;
+            }
+        }
+    }
+}
---
+++ Vendios/TimerManager/UtilitiesTimer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Vendios
+{
+
+    public class UtilitiesTimer
+    {
+        private DispatcherTimer _timer;
+        private int _seconds = 60;
+        private TextBlock _timerTextBlock;
+        private Frame _frame;
+
+
+        public UtilitiesTimer(TextBlock timerTextBlock, Frame frame)
+        {
+            _timerTextBlock = timerTextBlock;
+            _frame = frame;
+        }
+
+        /// <summary>
+        ///регистрируем фоновую задачу для отслеживания времени.
+        /// </summary>
+        public async void RegisterBackgroundTask()
+        {
+            var backgroundAccessStatus = await BackgroundExecutionManager.RequestAccessAsync();
+            if (backgroundAccessStatus == BackgroundAccessStatus.AlwaysAllowed)
+            {
+                var builder = new BackgroundTaskBuilder();
+                builder.Name = "TimerBackgroundTask";
+                builder.TaskEntryPoint = "VendiosRuntime.TimerBackgroundTask";
+                builder.SetTrigger(new TimeTrigger(60, false)); //триггер запускается через 60 секунд
+                builder.Register();
+            }
+        }
+
+        /// <summary>
+        ///запускаем таймер
+        /// </summary>
+        public void StartTimer()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        /// <summary>
+        ///обработчик события таймера. Вызывается каждую сек
+        /// </summary>
+        private void Timer_Tick(object sender, object e)
+        {
+            _seconds--;
+            UpdateTimerText();
+
+            if (_seconds <= 0)
+            {
+                _timer.Stop();
+                _timer = null;
+                _frame.Navigate(typeof(MainPage));
+            }
+        }
+
+        /// <summary>
+        ///обновляет текстбокс с отображаемым временем
+        /// </summary>
+        private void UpdateTimerText()
+        {
+            _timerTextBlock.Text = _seconds.ToString();
+        }
+    }
+}
